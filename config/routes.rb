c42ebb5707ff1cd6fe0eaceb7abd24efc7b369d8
mmmminamino <124233935+mmmminamino@users.noreply.github.com>
2023-04-14 08:20:06 +0000

    0414 commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root to: "homes#top"
+  get '/about'=>'homes#about', as:'about'
+  
   devise_for :customers,skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
@@ -8,4 +11,5 @@ Rails.application.routes.draw do
 }
   devise_for :items
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :customers, only: [:new, :create, :index, :show]
 end
