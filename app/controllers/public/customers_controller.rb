4633f33b28ae7377a90789a1714b7a5adc3855ed
mmmminamino <124233935+mmmminamino@users.noreply.github.com>
2023-04-21 11:22:55 +0000

    0421 commit

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -4,6 +4,16 @@ end
 def new
 end
 
+def create
+    @customer = Customer.new(customer_params)
+    @customer.id = current_user.id
+    if @customer.save
+      redirect_to customers_path
+    else
+      render :new
+    end
+end
+
 def show
     @customer=current_customer
 end
